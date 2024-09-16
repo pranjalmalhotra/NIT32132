232fe7f62c7d0234360ffbe3417cb3e6ec49b14f
Pranjal Malhotra <b.pranjal2003@gmail.com>
2024-09-16 23:50:43 +0500

    Login Screen
    Designed Login Screen

--- app/src/main/java/com/app/nit3213/MainActivity.kt
+++ app/src/main/java/com/app/nit3213/MainActivity.kt
@@ -8,50 +8,69 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.app.nit3213.databinding.ActivityMainBinding
 import com.app.nit3213.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val authViewModel: MainViewModel by viewModels()
+    private lateinit var binding:ActivityMainBinding
+    private val location = "sydney"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val location = "sydney"
+
+        binding.tvLogin.setOnClickListener {
+            validateFields()
+        }
         val username = "Pranjal"
         val password = "s4667717"
+    }
 
-        // Observe authentication result
+    private fun validateFields() {
+        val username = binding.etUsername.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+
+        if (username.isEmpty()) {
+            binding.etUsername.error = "Username cannot be empty"
+            return
+        }
+
+        if (password.isEmpty()) {
+            binding.etUsername.error = "Password cannot be empty"
+            return
+        }
+        loginUser(username,password)
+    }
+
+    private fun loginUser(username: String, password: String) {
+        Log.wtf("testing","$username $password $location")
+        authViewModel.login(location, username, password)
+        setUpObserver()
+    }
+
+    private fun setUpObserver() {
         authViewModel.authResult.observe(this) { authResponse ->
             if (authResponse != null) {
-                Log.wtf("checking","${authResponse.keypass}")
+                Toast.makeText(this, "${authResponse.keypass}", Toast.LENGTH_LONG).show()
             }
         }
 
-        authViewModel.isLoading.observe(this) { isLoading ->
-            Log.wtf("checking","${isLoading}")
+        authViewModel.isLoading.observe(this) { isLoading -> }
 
-        }
-
-        // Observe error messages
         authViewModel.errorMessage.observe(this) { error ->
-            Log.wtf("checking","${error}")
-
             if (error != null) {
-                // Show error message, e.g., using a Toast
-                Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_LONG).show()
             }
         }
-
-        // Trigger login when ready
-        authViewModel.login(location, username, password)
-
     }
 }
